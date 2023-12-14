8d7e36dd28639eb2070eb91b24ba89779090fef6
Josh Wills <josh.wills@gmail.com>
2023-12-14 10:05:57 -0800

    Question 7

---
+++ models/candles/seven.sql
@@ -0,0 +1,25 @@
+WITH customer6_orders AS (
+    SELECT o.* FROM {{ ref('stg_orders') }} o
+    INNER JOIN {{ ref('six') }} USING (customerid) 
+),
+customer6_color_orders_items AS (
+    SELECT oi.*
+    FROM {{ ref('stg_orders_items') }} oi
+    INNER JOIN customer6_orders o ON oi.orderid = o.orderid
+    WHERE sku LIKE 'COL%'
+),
+next_ordered_item AS (
+    SELECT oi.*
+    FROM customer6_color_orders_items c6oi
+    ASOF JOIN {{ ref('stg_orders_items') }} oi ON c6oi.orderid > oi.orderid
+    WHERE oi.sku LIKE 'COL%'
+    ORDER BY c6oi.orderid - oi.orderid
+),
+matching_order AS (
+    SELECT *
+    FROM {{ ref('stg_orders') }} o
+    INNER JOIN next_ordered_item noi ON o.orderid = noi.orderid
+)
+SELECT c.customerid, c.name, c.phone
+FROM {{ ref('stg_customers') }} c
+INNER JOIN matching_order mo ON c.customerid = mo.customerid
