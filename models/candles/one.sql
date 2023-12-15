e57d46ede2ca7e878eaf51f360e8c7a1769183ba
Josh Wills <josh.wills@gmail.com>
2023-12-15 11:08:35 -0800

    Simplify question one a bit

--- models/candles/one.sql
+++ models/candles/one.sql
@@ -2,12 +2,7 @@ WITH p1 AS (
     SELECT customerid
     , name
     , phone
-    , split_part(name, ' ', 2) as last_name
-    , split_part(phone, '-', 1) as area_code
-    , split_part(phone, '-', 2) as first_three_digits
-    , split_part(phone, '-', 3) as last_four_digits
     FROM {{ ref('stg_customers') }}
+    WHERE phone_numberize(split_part(name, ' ', 2)) = replace(phone, '-', '')
 )
-SELECT customerid, name, phone FROM p1
-WHERE (phone_numberize(last_name) = area_code || first_three_digits || last_four_digits)
-OR (phone_numberize(last_name) = first_three_digits || last_four_digits)
+SELECT * FROM p1
